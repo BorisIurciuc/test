8e528bf14c5ac59de20e56692372eb00f94f7d46
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-02 17:33:44 +0100

    add HelloGit.java

---
+++ HelloGit.java
@@ -0,0 +1,12 @@
+/**
+* 02/11/2023
+* lesson_23
+*
+* @author Boris Iurciuc (cohort36)
+*/
+public class HelloGit {
+
+  public static void main(String[] args) {
+    System.out.println("Hello git");
+  }
+}
