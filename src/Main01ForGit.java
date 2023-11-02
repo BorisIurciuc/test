d7b9c4bc9a91d30318195d19f456541d00f39934
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-02 21:58:34 +0100

    HelloGit.java add new sout. Main01ForGit new variable  int d

--- src/Main01ForGit.java
+++ src/Main01ForGit.java
@@ -7,12 +7,13 @@ public class Main01ForGit {
 
   public static void main(String[] args) {
 
-
-
   int a = 2;
   int b = 3;
   int s = a + b;
+  int d = a - b;
 
-  System.out.println("sum "+ s);
+  System.out.println("sum " + s);
+  System.out.println("div " + d);
   }
+
 }
