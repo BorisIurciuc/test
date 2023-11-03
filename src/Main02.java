f899211b939a7e43327a1d906250199179c63ff9
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-03 20:26:25 +0100

    addMain02

---
+++ src/Main02.java
@@ -0,0 +1,12 @@
+/**
+ * 03/11/2023 test @author Boris Iurciuc (cohort36)
+ */
+public class Main02 {
+
+  public static void main(String[] args) {
+    System.out.println("This is sample task");
+    System.out.println("Feature test");
+
+  }
+
+}
