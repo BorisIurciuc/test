6b3a9f8bc239dabfd06085ed96de6e50e516a4df
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-04 20:01:12 +0100

    Add new class Main03

---
+++ src/Main03.java
@@ -0,0 +1,12 @@
+/**
+ * 04/11/2023 test
+ *
+ * @author Boris Iurciuc (cohort36)
+ */
+public class Main03 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Main03");
+  }
+
+}
